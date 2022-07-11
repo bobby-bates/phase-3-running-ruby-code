d3af310946cb2a43c75cf8933095fd30becaf3a3
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-11 09:43:47 -0400

    completed assignment

---
+++ app.rb
@@ -0,0 +1,30 @@
+# # Print string, plus a line break:
+# puts 'Hello, World!'
+# puts 'Hello, World!'
+# puts 'Hello, World!'
+
+# # Print string, with NO line break:
+# print 'Hello, World!'
+# print 'Hello, World!'
+# print 'Hello, World!'
+
+# # Print each element of array and a line break:
+# puts [1, 2, 3]
+
+# # This is the equivalent of calling:
+# # puts [1, 2, 3].inspect
+# p [1, 2, 3]
+
+# # Pretty-print nested arrays and hashes (p will print on one line):
+# pp [
+#   { id: 1, hello: "World" },
+#   { id: 2, hello: "Ruby" },
+#   { id: 3, hello: "Moon" },
+#   { id: 4, hello: "Learner" }
+# ]
+
+puts 'Hello World!'
+
+print 'Pass this test, please.'
+
+p [1, 2, 3]
